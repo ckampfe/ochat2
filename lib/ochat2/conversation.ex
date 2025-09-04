045f57b5a6a599b2da303868272605ef374bd450
Clark Kampfe <clark.kampfe@gmail.com>
2025-09-03 23:36:36 -0500

    schema and migration for conversations

---
+++ lib/ochat2/conversation.ex
@@ -0,0 +1,19 @@
+defmodule Ochat2.Conversation do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "conversations" do
+    field :name, :string
+    field :model_id, :id
+    field :source_conversation_id, :id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(conversation, attrs) do
+    conversation
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
---
+++ priv/repo/migrations/20250904043328_create_conversations.exs
@@ -0,0 +1,16 @@
+defmodule Ochat2.Repo.Migrations.CreateConversations do
+  use Ecto.Migration
+
+  def change do
+    create table(:conversations) do
+      add :name, :string
+      add :model_id, references(:models, on_delete: :nothing)
+      add :source_conversation_id, references(:conversations, on_delete: :nothing)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:conversations, [:model_id])
+    create index(:conversations, [:source_conversation_id])
+  end
+end
